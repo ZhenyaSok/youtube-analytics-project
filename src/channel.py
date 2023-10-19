277bcd009550e3c6ae4458c5326e4f7030e348e1
Zhenya Osokina <kapitan.kub@gmail.com>
2023-10-19 16:35:50 +0500

    дз модуль 13.2, модифицируем модуль Channel в соответствии с заданием

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,49 @@
+import json
+import os
+from googleapiclient.discovery import build
 
 
 class Channel:
     """Класс для ютуб-канала"""
+    api_key: str = os.getenv('YOUTUBE_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.__channel_id = channel_id
+        self.channel = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        self.title = self.channel['items'][0]['snippet']['title']
+        self.description = self.channel['items'][0]['snippet']['description']
+        self.url = f"https://www.youtube/channels/{self.__channel_id}"
+        self.subscriber_count = int(self.channel['items'][0]['statistics']['subscriberCount'])
+        self.video_count = int(self.channel['items'][0]['statistics']['videoCount'])
+        self.view_count = int(self.channel['items'][0]['statistics']['viewCount'])
+
+    @classmethod
+    def get_service(cls):
+        """Выводит в консоль информацию о канале"""
+
+        api_key: str = os.getenv('YOUTUBE_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        return youtube
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+
+        print(json.dumps(self.youtube.channels().list(
+            part='snippet, contentDetails, statistics',
+            id=self.channel_id).execute(), indent=2, ensure_ascii=False))
+
+    def to_json(self, file_json):
+        """Сохраняет в файл значения атрибутов экземпляра Channel"""
+        json_data = {
+            'channel_id': self.__channel_id,
+            'title': self.title,
+            'descrition': self.description,
+            'url': self.url,
+            'subscriber_count': self.subscriber_count,
+            'videoCount': self.video_count,
+            'viewCount': self.view_count
+        }
+        with open(file_json, 'w', encoding="utf-8") as f:
+            f.write(json.dumps(json_data, ensure_ascii=False, indent=4))
